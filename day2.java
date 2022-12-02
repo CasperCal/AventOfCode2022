10eafdbd1a3f7cd9fae378f6f040aab7b5ef1804
Casper <89966510+CasperCal@users.noreply.github.com>
2022-12-02 13:36:07 +0100

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,40 @@
+class Scratch {
+    public static void main(String[] args) {
+
+    String[] splitInut = input.split("\n");
+        int result = 0;
+
+        for (int i = 0; i < splitInut.length; i++) {
+            result += calculatePointsForRound(splitInut[i]);
+        }
+        System.out.println(result);
+    }
+
+
+        private static int calculatePointsForRound (String splitInput){
+            int tempResult =  0;
+            if (splitInput.contains("X"))
+                tempResult = 1;
+            if (splitInput.contains("Y"))
+                tempResult = 2;
+            if (splitInput.contains("Z"))
+                tempResult = 3;
+            String[] charInput = splitInput.split(" ");
+            if (charInput[0].equals("A")) {
+                if (charInput[1].equals("X")) {tempResult += 3;}
+                if (charInput[1].equals("Y")) {tempResult += 6;}
+                if (charInput[1].equals("Z")) {tempResult += 0;}
+            }
+            if (charInput[0].equals("B")) {
+                if (charInput[1].equals("X")) {tempResult += 0;}
+                if (charInput[1].equals("Y")) {tempResult += 3;}
+                if (charInput[1].equals("Z")) {tempResult += 6;}
+            }
+            if (charInput[0].equals("C")) {
+                if (charInput[1].equals("X")) {tempResult += 6;}
+                if (charInput[1].equals("Y")) {tempResult += 0;}
+                if (charInput[1].equals("Z")) {tempResult += 3;}
+            }
+            return tempResult;
+        }
+    }
